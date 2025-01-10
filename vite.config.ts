e319615491f64d2cadc3c497e2eec5c3458f2d90
Vitor Martins <94311178+martinsvitor@users.noreply.github.com>
2025-01-10 19:17:46 +0100

    Dev (#14)
    * add missing dependency, change config file
    
    * add missing dependency, change config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      enabled: true,
+      reportsDirectory:'./tests/unit/coverage'
+    },
+  },
+// @ts-ignore
   plugins: [react()],
+
 });
